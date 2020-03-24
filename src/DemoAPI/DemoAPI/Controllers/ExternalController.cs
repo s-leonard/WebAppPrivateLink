fb0530a8c1e7f03941eab512657e4b88b8b2b7c8
Steve Leonard <sleonard84@msn.com>
2020-03-24 22:17:43 +0000

    api external call

--- src/DemoAPI/DemoAPI/Controllers/ExternalController.cs
+++ src/DemoAPI/DemoAPI/Controllers/ExternalController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -9,31 +10,19 @@ namespace DemoAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ExternalController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+      
+        public ExternalController()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<string> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync("https://raw.githubusercontent.com/s-leonard/WebAppPrivateLink/master/README.md");
+            return response;
         }
     }
 }
