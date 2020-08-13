21f808323b50bd017ca14bec7bd69e66631c304b
Steve Leonard <sleonard84@msn.com>
2020-08-13 17:29:43 +0100

    firewall

--- HubAndSpokeNetwork/CLI/script.sh
+++ HubAndSpokeNetwork/CLI/script.sh
@@ -1,10 +1,13 @@
 
+## Pre-Req
+az extension add --name azure-firewall
+
 ###########################################
 # Global Variables
 ###########################################
 LOCATION=centralus
 PREFIX=WS$(date +%s%N | md5sum | cut -c1-6)
-
+SUBID=$(az account list --query "[?isDefault].id" -o tsv)
 
 ###########################################
 # Hub & Spoke Networks
@@ -36,8 +39,9 @@ API_SUBNET_PROD=apiprod
 API_SUBNET_IPRANGE_DEV=10.1.2.0/24
 API_SUBNET_IPRANGE_PROD=10.2.2.0/24
 
-#FIREWALL_SUBNET=firewall
-#FIREWALL_SUBNET_IPRANGE=10.0.0.0/24
+#Firewall subnet MUST be named AzureFirewallSubnet
+FIREWALL_SUBNET=AzureFirewallSubnet
+FIREWALL_SUBNET_IPRANGE=10.0.0.0/24
 
 HUB_TO_SPOKE_VNET_PEER_DEV=$(echo $PREFIX)-hub-spoke-peer-dev
 SPOKE_TO_HUB_VNET_PEER_DEV=$(echo $PREFIX)-spoke-hub-peer-dev
@@ -62,7 +66,7 @@ az network vnet subnet create -n $WEB_SUBNET_PROD -g $RG_SPOKE_PROD --address-pr
 az network vnet subnet create -n $API_SUBNET_DEV -g $RG_SPOKE_DEV --address-prefixes $API_SUBNET_IPRANGE_DEV --vnet-name $VNET_SPOKE_DEV --service-endpoints "Microsoft.Web"
 az network vnet subnet create -n $API_SUBNET_PROD -g $RG_SPOKE_PROD --address-prefixes $API_SUBNET_IPRANGE_PROD --vnet-name $VNET_SPOKE_PROD --service-endpoints "Microsoft.Web"
 
-#az network vnet subnet create -n $FIREWALL_SUBNET -g $RG_HUB --address-prefixes $FIREWALL_SUBNET_IPRANGE --vnet-name $VNET_HUB
+az network vnet subnet create -n $FIREWALL_SUBNET -g $RG_HUB --address-prefixes $FIREWALL_SUBNET_IPRANGE --vnet-name $VNET_HUB
 
 #Peer the Vnets
 HUBID=$(az network vnet show -g $RG_HUB -n $VNET_HUB --query id -o tsv)
@@ -78,18 +82,58 @@ az network vnet peering create -g $RG_SPOKE_PROD -n $SPOKE_TO_HUB_VNET_PEER_PROD
 ###########################################
 # Hub Network
 ###########################################
-
-RG_HUB="hub"
-VNET_HUB="hub"
-
-## Hub VNet
-az network vnet create -n $VNET_HUB -g $RG_HUB --address-prefixes 10.0.0.0/16
-
+FWPUBLICIP_NAME=$(echo $PREFIX)-fw-ip
+FWNAME=$(echo $PREFIX)-fw
+FWROUTE_TABLE_NAME="${PREFIX}fwrt"
+FWROUTE_NAME="${PREFIX}fwrn"
+FWROUTE_NAME_INTERNET="${PREFIX}fwinternet"
+FWIPCONFIG_NAME="${PREFIX}fwconfig"
 
 ## Firewall
 
-az network vnet subnet create -n $FIREWALL_SUBNET -g $RG_HUB \
-    --address-prefixes 10.0.0.0/24 --vnet-name $VNET_HUB
+az network public-ip create -g $RG_HUB -n $FWPUBLICIP_NAME -l $LOCATION --sku "Standard"
+
+az network firewall create -g $RG_HUB -n $FWNAME -l $LOCATION
+
+# Configure Firewall IP Config
+
+az network firewall ip-config create \
+    -g $RG_HUB \
+    -f $FWNAME \
+    -n $FWIPCONFIG_NAME \
+    --public-ip-address $FWPUBLICIP_NAME \
+    --vnet-name $VNET_HUB
+
+# Capture Firewall IP Address for Later Use
+
+FWPUBLIC_IP=$(az network public-ip show -g $RG_HUB -n $FWPUBLICIP_NAME --query "ipAddress" -o tsv)
+FWPRIVATE_IP=$(az network firewall show -g $RG_HUB -n $FWNAME --query "ipConfigurations[0].privateIpAddress" -o tsv)
+
+# Create UDR and add a route for the web subnet (spoke), this ensures all traffic from the web app goes through the firewall
+az network route-table create -g $RG_SPOKE_DEV --name $FWROUTE_TABLE_NAME
+az network route-table route create -g $RG_SPOKE_DEV --name $FWROUTE_NAME --route-table-name $FWROUTE_TABLE_NAME --address-prefix 0.0.0.0/0 --next-hop-type VirtualAppliance --next-hop-ip-address $FWPRIVATE_IP --subscription $SUBID
+
+az network route-table create -g $RG_SPOKE_PROD --name $FWROUTE_TABLE_NAME
+az network route-table route create -g $RG_SPOKE_PROD --name $FWROUTE_NAME --route-table-name $FWROUTE_TABLE_NAME --address-prefix 0.0.0.0/0 --next-hop-type VirtualAppliance --next-hop-ip-address $FWPRIVATE_IP --subscription $SUBID
+
+
+#Ensure all traffic to httpbin.org is allowed (highly locked down)
+#az network firewall application-rule create \
+#    --collection-name $FIREWALL_HTTPBIN_APPLICATION_RULE_COLLECTION \
+#    --name $FIREWALL_HTTPBIN_APPLICATION_RULE \
+#    --firewall-name $FWNAME \
+#    -g $RG_HUB \
+#    --protocols HTTP=80 HTTPS=443 \
+#    --action Allow \
+#    --priority 100 \
+#    --target-fqdns "httpbin.org" \
+#    --source-addresses "*"
+
+#Add the UDR to the network
+az network vnet subnet update -g $RG_SPOKE_DEV --vnet-name $VNET_SPOKE_DEV --name $WEB_SUBNET_DEV --route-table $FWROUTE_TABLE_NAME
+az network vnet subnet update -g $RG_SPOKE_DEV --vnet-name $VNET_SPOKE_DEV --name $API_SUBNET_DEV --route-table $FWROUTE_TABLE_NAME
+az network vnet subnet update -g $RG_SPOKE_PROD --vnet-name $VNET_SPOKE_PROD --name $WEB_SUBNET_PROD --route-table $FWROUTE_TABLE_NAME
+az network vnet subnet update -g $RG_SPOKE_PROD --vnet-name $VNET_SPOKE_PROD --name $API_SUBNET_PROD --route-table $FWROUTE_TABLE_NAME
 
 ## Azure Deplopyment Agent
 
